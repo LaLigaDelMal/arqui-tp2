e4551548e5a6fc506f893a3f696be74cebd6d82c
Adriel Chagay <adrielchagay@gmail.com>
2022-12-02 16:59:08 -0300

    Update fifo.v

--- src/fifo.v
+++ src/fifo.v
@@ -1,15 +1,16 @@
 `timescale 1ns / 1ps
 
 module fifo #(
-		parameter WORD_SIZE = 8, //Columnas
+		parameter DATA_WIDTH = 8, //Columnas
 		parameter LENGTH = 4	 //Filas
 	)(
-		input wire i_clk, i_rst,
+	   //Relevant for Reading: i_read, i_data, o_empty
+		input wire i_clock, i_reset,
 		input wire i_read, i_write,
-		input wire [WORD_SIZE-1:0] i_data,
+		input wire [DATA_WIDTH-1:0] i_data, //For Writing to FIFO
 		
 		output wire o_empty, o_full,
-		output wire [WORD_SIZE-1:0] o_data
+		output wire [DATA_WIDTH-1:0] o_data //For Reading from FIFO
 	);
 	
 	localparam CTR_SZ = $clog2(LENGTH);
@@ -17,38 +18,44 @@ module fifo #(
 	reg empty;
 	reg full;
 	
-	reg [WORD_SIZE-1:0] dataOut;
+	reg [DATA_WIDTH-1:0] dataOut;
 
-	reg [WORD_SIZE-1:0] FIFO[LENGTH-1:0];
+	reg [DATA_WIDTH-1:0] FIFO[LENGTH-1:0];
 	reg [LENGTH-1:0] FIFO_status;
 	
 	reg [CTR_SZ-1:0] index_read, index_write;
+	reg [CTR_SZ-1:0] next_index_read, next_index_write;
 
 	integer i;
 
     //RESET
-	always @(posedge i_clk) begin
-		if (i_rst) begin
-			FIFO_status <= {LENGTH{1'b0}};
-			full <= 1'b0;
-			empty <= 1'b1;
-			index_read <= {CTR_SZ{1'b0}};
-			index_write <= {CTR_SZ{1'b0}};
-			dataOut <= {WORD_SIZE{1'b0}};
+	always @(*) begin
+        if (i_reset) begin
+            FIFO_status <= {LENGTH{1'b0}};
+            full <= 1'b0;
+            empty <= 1'b1;
+            index_read <= {CTR_SZ{1'b0}};
+            index_write <= {CTR_SZ{1'b0}};
+            next_index_read <= {CTR_SZ{1'b0}};
+            next_index_write <= {CTR_SZ{1'b0}};
+		end
+		else begin
+            index_write <= next_index_write;
+            index_read <= next_index_read;
 		end
 	end
 
     //READING
-	always @(posedge i_clk) begin
+	always @(posedge i_clock) begin
 		if(i_read && FIFO_status[index_read]) begin
-			dataOut = FIFO[index_read];
+			dataOut <= FIFO[index_read];
 			FIFO_status[index_read] <= 1'b0;
-			index_read <= index_read + 1;
+			next_index_read <= index_read + 1;
 
 			full <= 1'b0;
 
 			if(index_read == LENGTH) begin
-				index_read <= 0;
+				next_index_read <= 0;
 			end 
 		end
 
@@ -59,22 +66,21 @@ module fifo #(
 
     //WRITING
     // When flag full is set you cannot write to FIFO you fool
-	always @(negedge i_clk) begin
+	always @(posedge i_clock) begin
 		
 		if(i_write && ~FIFO_status[index_write]) begin
 			FIFO[index_write] <= i_data;
 			FIFO_status[index_write] <= 1'b1;
-			index_write <= index_write + 1;
+			next_index_write <= index_write + 1;
 
 			empty <= 0;
 			
 			if(index_write == LENGTH) begin
-				index_write <= 0;
+				next_index_write <= 0;
 			end 
 		end
 		
 		if(&FIFO_status) begin
-		//if( &( FIFO_status ~^ {LENGTH{1'b1}}) ) begin
 			full <= 1'b1;
 		end
 
